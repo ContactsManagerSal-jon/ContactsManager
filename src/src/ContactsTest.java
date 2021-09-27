07a4dc63bd0d724404b36b9818101f65637e0b2c
salvador salazar <salvador.g.salazar79@gmail.com>
2021-09-27 09:57:15 -0500

    adding udpated classes

--- src/src/ContactsTest.java
+++ src/src/ContactsTest.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Array;
@@ -8,57 +9,102 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class ContactsTest {
-    public void createContact(){
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Contacts chucky = new Contacts();
+
+
+
+        boolean userContinue = true;
+        do{
+            System.out.println("1. View contacts.\n" +
+                    "2. Add a new contact.\n" +
+                    "3. Search a contact by name.\n" +
+                    "4. Delete an existing contact.\n" +
+                    "5. Exit.\n" +
+                    "Enter an option (1, 2, 3, 4 or 5):");
+
+            int input = scanner.nextInt();
+
+
+
+//            int userSelection = contactsMenu();
+            switch(input) {
+                case 1:
+                    chucky.showContacts();
+                    break;
+                case 2:
+                    chucky.addContact();
+                    break;
+                case 3:
+                    chucky.editContact();
+                    break;
+                case 4:
+                    chucky.deleteContact();
+                    break;
+                case 5:
+                    System.err.println("Exiting program. Have a great day!");
+                    userContinue = false;
+                    break;
+                default:
+                    System.out.println("Try selecting a different number.");
+            }
+            if(userContinue){
+                System.out.println();
+                //  userContinue = yesNo("Would you like to continue? (Y/N)");
+
+
+            }
+        }   while(userContinue);
+
+
+//        //      //-----load all contats tetst
+//        System.out.println("Current Address Book: ");
+//        chucky.showContacts();
+//        System.out.println("");
+//
+////      //----add contacts test
+//        chucky.addContact();
+//        System.out.println("Updated address book:");        chucky.showContacts();
+
+
+////      // -------edit test
+//        chucky.editContact();
+//        System.out.println("Current Address Book after Edit: ");
+//        chucky.showContacts();
+
+
+
+//        //--------delete test
+//        chucky.deleteContact();
+//        System.out.println("Current Address Book after DELETE: ");
+//        chucky.showContacts();
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
-//------------addcontact method Work in Progress
-//    public static void addContacts(Path contacts,String name,String number){
-//        List<String> contactsList = Arrays.asList(name);
-//        try{
-//            Files.write(contacts,contactsList, StandardOpenOption.APPEND.())
-//        }catch (Exception e){
-//            e.printStackTrace();
-//
-//        }
-//    }
+}
 
 
-
-    //public static List<Contacts> loadContacts(List<Contact>)
-
-    public static void main(String[] args) {
-        Contacts chucky =  new Contacts("name1","1235");
-        for (String loadContact : chucky.loadContacts()) {
-            System.out.println(loadContact);
-        }
-
-
-        List<Contacts> contacts = new ArrayList<>();
-        Path contactsPath = Paths.get("/Users/Jonathan/IdeaProjects/ContactsManager/src", "contact.txt");
-
-        List<String> contactsList = Arrays.asList("Jon doe", "Jane doe");
-        try{
-            Files.write(contactsPath,contactsList);
-        }catch(IOException xd){
-            System.out.println("It requires this useless crap");
-        }
-//
-//        List<String> currentList = new ArrayList<>();
-//        try{
-//            currentList = Files.readAllLines(contactsPath);
-//        }catch(IOException stupid){
-//            System.out.println("Catching This Nonsense");
-//        }
-//        for(String l : currentList){
-//            System.out.println(l);
-        }
-
-
-
-
-    }
-
-//}
